aa5f0d22a44b27bde7799844e1360acd13ef2584
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2019-03-24 15:36:55 +0800

    added key frames animation

--- Flight Info/ViewController.swift
+++ Flight Info/ViewController.swift
@@ -92,13 +92,75 @@ class ViewController: UIViewController {
         
     }
     
-    func cubeTransition() {
+    func cubeTransition(label: UILabel, text: String) {
         //TODO: Create a faux rotating cube animation
+        //Create and setup temp label
+        let tempLabel = duplicateLabel(label: label)
+        tempLabel.text = text
+        
+        let tempLabelOffset = label.frame.size.height / 2.0
+        let scale = CGAffineTransform(scaleX: 1.0, y: 0.1)
+        let translate = CGAffineTransform(translationX: 0.0, y: tempLabelOffset)
+        
+        tempLabel.transform = translate.concatenating(scale)
+        label.superview?.addSubview(tempLabel)
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.0,
+                       options: .curveEaseOut, animations: {
+                        //Scale temp labbel down and translate up
+                        tempLabel.transform = .identity
+                        //Scale real labbel down and translate up
+                        label.transform = translate.inverted().concatenating(scale)
+        }) {_ in
+            //Update the real label's text and reset its transform
+            label.text = tempLabel.text
+        }
     }
     
     
     func planeDepart() {
         //TODO: Animate the plane taking off and landing
+        
+        //Store the plane's center value
+        let originalCenter = planeImage.center
+        
+        //Create a new keyframe animation
+        UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, options: [], animations: {
+            //Move plane to the right and up
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25, animations: {
+                self.planeImage.center.x += 80.0
+                self.planeImage.center.y -= 10.0
+            })
+            
+            //Rotate plane
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4) {
+                self.planeImage.transform = CGAffineTransform(rotationAngle: -.pi / 8.0)
+            }
+            
+            //Move plane right and up off screen, fade out
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25, animations: {
+                self.planeImage.center.x += 100.0
+                self.planeImage.center.y -= 50.0
+                self.planeImage.alpha = 0.0
+            })
+            
+            //Move plane off left side of screen, reset transform and height
+            UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01, animations: {
+                self.planeImage.transform = .identity
+                self.planeImage.center = CGPoint(x: 0.0, y: originalCenter.y)
+            })
+            
+            //Move plane back to original position and fade in
+            UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45, animations: {
+                self.planeImage.alpha = 1.0
+                self.planeImage.center = originalCenter
+            })
+            
+        }, completion: nil)
+        
+
+        
     }
     
     func changeSummary(to summaryText: String) {
@@ -129,20 +191,33 @@ class ViewController: UIViewController {
             
             // pass animating properties here to change label
             let offset = CGPoint(x: -80.0, y: 0.0)
-            moveLabel(label: arrivingTo, text: data.arrivingTo, offset: offset)
+            //moveLabel(label: flightNr, text: data.flightNr, offset: offset)
+            //moveLabel(label: gateNr, text: data.gateNr, offset: offset)
             
             //TODO: Add Variety to Animating Properties
             let offsetArriving = CGPoint(x: 0.0, y: data.showWeatherEffects ? 50.0 : -50.0)
             moveLabel(label: arrivingTo, text: data.arrivingTo, offset: offsetArriving)
             
+            
             let offsetDeparting = CGPoint(x: data.showWeatherEffects ? 80.0 : -80.0, y: 0.0)
             moveLabel(label: departingFrom, text: data.departingFrom, offset: offsetDeparting)
+            
+            cubeTransition(label: flightStatus, text: data.flightStatus)
+            cubeTransition(label: flightNr, text: data.flightNr)
+            cubeTransition(label: gateNr, text: data.gateNr)
+            
+            planeDepart()
             
         } else {
             bgImageView.image = UIImage(named: data.weatherImageName)
             snowView.isHidden = !data.showWeatherEffects
+            
             departingFrom.text = data.departingFrom
             arrivingTo.text = data.arrivingTo
+            
+            flightStatus.text = data.flightStatus
+            flightNr.text = data.flightNr
+            gateNr.text = data.gateNr
         }
         
         // schedule next flight
